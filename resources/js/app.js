fd0fa323d85cac2d933c98c8421dea5ee7d81936
sina1010anis <sina1010anis@gmail.com>
2023-06-30 10:16:39 +0330

    setup project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import { createApp, h } from "vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import  Version from './components/Version'
+
+
+const app = createApp({
+    data:()=>({
+        text:'Vue'
+    }),
+    components:{
+        Version
+    }
+})
+
+
+app.mount('#app')
