92a10f431477fed8debc43033d5e04b8b4b67623
sina1010 <sina1010anis@gmail.com>
2024-11-15 14:38:30 +0330

    new loding

--- resources/views/seting/page.blade.php
+++ resources/views/seting/page.blade.php
@@ -11,40 +11,8 @@
     </head>
     <body class="antialiased">
                     {{-- Loding Start --}}
-                    @if ($seting->where('type' , 'loding')->first()->status == 1)
-                    <div class="loding" id="loding_id" style="height: 100vh;z-index: 20;position:fixed;top:0;left:0;width: 100%;background-color: white">
-                        <img id="img_loding" src="/storage/images/v.jpg" alt="logo" style="">
-                    </div>
-                    <style>
-                        #img_loding{
-                            transform: translate(-50%,-50%);position: absolute;top:50%;right: 50%;width: 75px;transition: 0.2s;
-                            animation: loding 3.5s linear forwards;
-                        }
-                        @keyframes loding{
-                            0%{
-                                transform: scale(1)
-                            }
-                            25%{
-                                transform: scale(0.75)
-                            }
-                            50%{
-                                transform: scale(0.50)
-                            }
-                            75%{
-                                transform: scale(0.25)
-                            }
-                            100%{
-                                transform: scale(0)
-                            }
-                        }
-                    </style>
-                    <script>
-                        setTimeout(()=>{
-                            document.getElementById("loding_id").style.display = "none";
-                        } , 1500)
-                    </script>
-                {{-- Loding End --}}
-                    @endif
+                    <x-load-page />
+
         <div id="app">
             <div class="container-xxl">
                 <div class="row">
